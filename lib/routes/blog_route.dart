451eeebe6107acff163eb4283360d949d60f3519
daiki0210 <daidaidai171@gmail.com>
2020-10-25 20:33:00 +0900

    プログレスインジゲーター追加

--- lib/routes/blog_route.dart
+++ lib/routes/blog_route.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+// import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
-class Blog extends StatelessWidget {
+class Blog extends StatefulWidget {
+  @override
+  _BlogState createState() => _BlogState();
+}
+
+class _BlogState extends State<Blog> {
   // This widget is the root of your application.
+  String title;
+  var url = 'https://prog-ac.hatenablog.com/';
+  bool isLoading = true;
+  final _key = UniqueKey();
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(routes: {
-      "/": (_) => new WebviewScaffold(
-            url: "https://prog-ac.hatenablog.com/",
-            appBar: new AppBar(
-              title: new Text("ブログ"),
-            ), // AppBar
-          ) // WebviewScaffold
-    }); // MaterialApp
+    return Scaffold(
+      appBar: AppBar(
+          title:
+             const Text('ブログ', style: TextStyle(fontWeight: FontWeight.w700)),
+          centerTitle: true),
+      body: Stack(
+        children: <Widget>[
+          WebView(
+            key: _key,
+            initialUrl: url,
+            javascriptMode: JavascriptMode.unrestricted,
+            onPageFinished: (finish) {
+              setState(() {
+                isLoading = false;
+              });
+            },
+          ),
+          isLoading
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Stack(),
+        ],
+      ),
+    );
   }
 }
