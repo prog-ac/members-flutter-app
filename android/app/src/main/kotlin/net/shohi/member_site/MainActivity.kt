2a9add000eaefff41166b504996a54fbb16a4d05
ide Shouhi <shouhi0922@icloud.com>
2020-10-26 18:14:00 +0900

    スプラッシュウィンドウ実装しました

--- android/app/src/main/kotlin/net/shohi/member_site/MainActivity.kt
+++ android/app/src/main/kotlin/net/shohi/member_site/MainActivity.kt
@@ -2,5 +2,8 @@ package net.shohi.member_site
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
