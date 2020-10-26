3a21d15f44267fbf15b20abffb69eabde2e3a67e
ide Shouhi <shouhi0922@icloud.com>
2020-10-26 19:22:37 +0900

    デバッグバナー削除しておきました

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
         home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
